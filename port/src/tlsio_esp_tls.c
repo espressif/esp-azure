espressif/esp-azure

commit 2ed208ca7368a3998d87b690c37ae5b4d1a2bb48

tlsio: cap rcv_bytes loop
In this implementation of the TLS layer for the dowork_read, there is a potential for the rcv_bytes loop to go on continuously without breaking. As long as data continues to stream into the socket, the esp_tls_conn_read will return a non-zero number for rcv_bytes. To prevent this from occurring and allowing other processes such as a watchdog to execute, we set a cap of 5 iterations through the loop, at which point we will break and continue on to other tasks before the next dowork call.

Merges https://github.com/espressif/esp-azure/pull/28

port/src/tlsio_esp_tls.c
@@ -36,6 +36,7 @@ typedef struct
 // to minimize memory consumption.
 #define TLSIO_RECEIVE_BUFFER_SIZE 64
 
+#define MAX_RCV_COUNT 5
 
 typedef enum TLSIO_STATE_TAG
 {
@@ -394,11 +395,12 @@ static int dowork_read(TLS_IO_INSTANCE* tls_io_instance)
     // rather than adding to heap fragmentation.
     unsigned char buffer[TLSIO_RECEIVE_BUFFER_SIZE];
     int rcv_bytes = 0;
+    int rcv_count = 0;
 
     if (tls_io_instance->tlsio_state == TLSIO_STATE_OPEN)
     {
         rcv_bytes = esp_tls_conn_read(tls_io_instance->esp_tls_handle, buffer, sizeof(buffer));
-        while (rcv_bytes > 0)
+        while (rcv_bytes > 0 && rcv_count++ < MAX_RCV_COUNT)
         {
             // tls_io_instance->on_bytes_received was already checked for NULL
             // in the call to tlsio_esp_tls_open_async

commit 4c5a34f093316d522805c2e3ace7e568f5686433

tlsio: Fixed issue caused by commit 2ed208c.
A large message will cause incorrect read data from TLS due to skip of TLS read data, if required "TLS read data loops" for message is more than 5.

Closes https://github.com/espressif/esp-azure/pull/39

Signed-off-by: Jitin George <jitin@espressif.com>

port/src/tlsio_esp_tls.c
@@ -400,12 +400,20 @@ static int dowork_read(TLS_IO_INSTANCE* tls_io_instance)
     if (tls_io_instance->tlsio_state == TLSIO_STATE_OPEN)
     {
         rcv_bytes = esp_tls_conn_read(tls_io_instance->esp_tls_handle, buffer, sizeof(buffer));
-        while (rcv_bytes > 0 && rcv_count++ < MAX_RCV_COUNT)
+        while (rcv_bytes > 0)
         {
             // tls_io_instance->on_bytes_received was already checked for NULL
             // in the call to tlsio_esp_tls_open_async
             /* Codes_SRS_TLSIO_30_100: [ As long as the TLS connection is able to provide received data, tlsio_dowork shall repeatedly read this data and call on_bytes_received with the pointer to the buffer containing the data, the number of bytes received, and the on_bytes_received_context. ]*/
             tls_io_instance->on_bytes_received(tls_io_instance->on_bytes_received_context, buffer, rcv_bytes);
+            
+            if (++rcv_count > MAX_RCV_COUNT)
+            {
+                // Read no more than "MAX_RCV_COUNT" times to avoid starvation of other processes.
+                // LogInfo("Skipping further reading to avoid starvation.");
+                break;
+            }
+            
             rcv_bytes = esp_tls_conn_read(tls_io_instance->esp_tls_handle, buffer, sizeof(buffer));
         }
         /* Codes_SRS_TLSIO_30_102: [ If the TLS connection receives no data then tlsio_dowork shall not call the on_bytes_received callback. ]*/