1cdedb25b796894230e36134d9bbc77a683a2171
Alex Belousov <alex.blsv@gmail.com>
2022-06-02 19:21:37 +0300

    add more NTP servers, improve NTP get time, remove warning regarding wifi calibration

--- port/src/agenttime_esp.c
+++ port/src/agenttime_esp.c
@@ -20,28 +20,28 @@
 
 void initialize_sntp(void)
 {
-    printf("Initializing SNTP\n");
-    sntp_setoperatingmode(SNTP_OPMODE_POLL);
-    sntp_setservername(0, "pool.ntp.org");
-    sntp_init();
+//    printf("Initializing SNTP\n");
+//    sntp_setoperatingmode(SNTP_OPMODE_POLL);
+//    sntp_setservername(0, "pool.ntp.org");
+//    sntp_init();
 }
 
 static void obtain_time(void)
 {
-    // wait for time to be set
-    time_t now = 0;
-    struct tm timeinfo = { 0 };
-    int retry = 0;
-
-    printf("Waiting for system time to be set...\r\n");
-    while(timeinfo.tm_year < (2016 - 1900) ) {
-        retry++;
-//        printf("Waiting for system time to be set... tm_year:%d[times:%d]\n", timeinfo.tm_year, ++retry);
-        vTaskDelay(2000 / portTICK_PERIOD_MS);
-        time(&now);
-        localtime_r(&now, &timeinfo);
-    }
-    printf("system time is set\r\n");
+//    // wait for time to be set
+//    time_t now = 0;
+//    struct tm timeinfo = { 0 };
+//    int retry = 0;
+//
+//    printf("Waiting for system time to be set...\r\n");
+//    while(timeinfo.tm_year < (2016 - 1900) ) {
+//        retry++;
+////        printf("Waiting for system time to be set... tm_year:%d[times:%d]\n", timeinfo.tm_year, ++retry);
+//        vTaskDelay(2000 / portTICK_PERIOD_MS);
+//        time(&now);
+//        localtime_r(&now, &timeinfo);
+//    }
+//    printf("system time is set\r\n");
 }
 
 time_t sntp_get_current_timestamp()
@@ -52,8 +52,10 @@ time_t sntp_get_current_timestamp()
 	localtime_r(&now, &timeinfo);
 	// Is time set? If not, tm_year will be (1970 - 1900).
 	if (timeinfo.tm_year < (2016 - 1900)) {
-		printf("Time is not set yet. Connecting to WiFi and getting time over NTP. timeinfo.tm_year:%d\n",timeinfo.tm_year);
-		obtain_time();
+
+//        printf("WARNING!!! ToDo: Need use @ref azure_client_check_time\r\n Time is not set yet. Connecting to WiFi and getting time over NTP. timeinfo.tm_year:%d\n",timeinfo.tm_year);
+//		obtain_time();
+
 		// update 'now' variable with current time
 		time(&now);
 	}
@@ -64,7 +66,6 @@ time_t sntp_get_current_timestamp()
 time_t get_time(time_t* currentTime)
 {
     return sntp_get_current_timestamp();
-
 }
 
 double get_difftime(time_t stopTime, time_t startTime)
