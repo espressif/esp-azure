66a029aaff46a82a0234c69d936095bbddd344ad
Sylvain Giroudon <sylvain.giroudon@goobie.fr>
2023-03-10 17:42:30 +0100

    Support esp-idf v5.0

--- port/src/tlsio_esp_tls.c
+++ port/src/tlsio_esp_tls.c
@@ -22,6 +22,7 @@
 #include "azure_c_shared_utility/tlsio_options.h"
 #include "azure_c_shared_utility/shared_util_options.h"
 
+#include "esp_system.h"
 #include "esp_tls.h"
 
 typedef struct
@@ -70,7 +71,6 @@ typedef struct TLS_IO_INSTANCE_TAG
     char* hostname;
     SINGLYLINKEDLIST_HANDLE pending_transmission_list;
     TLSIO_OPTIONS options;
-    bool set_renegotiation;
 } TLS_IO_INSTANCE;
 
 /* Codes_SRS_TLSIO_30_005: [ The phrase "enter TLSIO_STATE_EXT_ERROR" means the adapter shall call the on_io_error function and pass the on_io_error_context that was supplied in tlsio_open_async. ]*/
@@ -127,7 +127,11 @@ static void internal_close(TLS_IO_INSTANCE* tls_io_instance)
     /* Codes_SRS_TLSIO_30_006: [ The phrase "enter TLSIO_STATE_EXT_CLOSED" means the adapter shall forcibly close any existing connections then call the on_io_close_complete function and pass the on_io_close_complete_context that was supplied in tlsio_close_async. ]*/
     /* Codes_SRS_TLSIO_30_051: [ On success, if the underlying TLS does not support asynchronous closing, then the adapter shall enter TLSIO_STATE_EXT_CLOSED immediately after entering TLSIO_STATE_EX_CLOSING. ]*/
 
+#if (ESP_IDF_VERSION < ESP_IDF_VERSION_VAL(4, 0, 0))
     esp_tls_conn_delete(tls_io_instance->esp_tls_handle);
+#else
+    esp_tls_conn_destroy(tls_io_instance->esp_tls_handle);
+#endif
     while (process_and_destroy_head_message(tls_io_instance, IO_SEND_CANCELLED));
     // singlylinkedlist_destroy gets called in the main destroy
 
@@ -219,7 +223,11 @@ static CONCRETE_IO_HANDLE tlsio_esp_tls_create(void* io_create_parameters)
                 result->pending_transmission_list = NULL;
                 tlsio_options_initialize(&result->options, TLSIO_OPTION_BIT_TRUSTED_CERTS |
                 TLSIO_OPTION_BIT_x509_RSA_CERT | TLSIO_OPTION_BIT_x509_ECC_CERT);
+#if (ESP_IDF_VERSION < ESP_IDF_VERSION_VAL(4, 0, 0))
                 result->esp_tls_handle = calloc(1, sizeof(esp_tls_t));
+#else
+                result->esp_tls_handle = esp_tls_init();
+#endif
                 if (result->esp_tls_handle == NULL)
                 {
                     /* Codes_SRS_TLSIO_30_011: [ If any resource allocation fails, tlsio_create shall return NULL. ]*/
@@ -485,10 +493,6 @@ static void tlsio_esp_tls_dowork(CONCRETE_IO_HANDLE tls_io)
             {
             int result = esp_tls_conn_new_async(tls_io_instance->hostname, strlen(tls_io_instance->hostname), tls_io_instance->port, &tls_io_instance->esp_tls_cfg, tls_io_instance->esp_tls_handle);
             if (result == 1) {
-                if (tls_io_instance->set_renegotiation) {
-                    //ESP_LOGI("esp-azure", "TLSIO_STATE_INIT: Set renegotiation");
-                    mbedtls_ssl_conf_renegotiation(&tls_io_instance->esp_tls_handle->conf, MBEDTLS_SSL_RENEGOTIATION_ENABLED);
-                }
                 tls_io_instance->tlsio_state = TLSIO_STATE_OPEN;
                 tls_io_instance->on_open_complete(tls_io_instance->on_open_complete_context, IO_OPEN_OK);
             } else if (result == -1) {
@@ -642,11 +646,14 @@ static int tlsio_esp_tls_setoption(CONCRETE_IO_HANDLE tls_io, const char* option
             {
                 if (strcmp(optionName, OPTION_SET_TLS_RENEGOTIATION) == 0)
                 {
-                    tls_io_instance->set_renegotiation = *((bool*)(value));
-                    //ESP_LOGI("esp-azure", "Set renegotiation to : %u", tls_io_instance->set_renegotiation);
-                    mbedtls_ssl_conf_renegotiation(&tls_io_instance->esp_tls_handle->conf,
-                                                   tls_io_instance->set_renegotiation ? MBEDTLS_SSL_RENEGOTIATION_ENABLED : MBEDTLS_SSL_RENEGOTIATION_DISABLED);
+                    /* esp_tls renegociation should be statically enabled in build config,
+                       menu Component config -> mbedTLS -> Support TLS renegotiation */
+#if CONFIG_MBEDTLS_SSL_RENEGOTIATION
                     result = 0;
+#else
+                    #warning MBEDTLS_SSL_RENEGOTIATION should be enabled in menuconfig
+                    result = MU_FAILURE;
+#endif
                 }
                 else
                 {
